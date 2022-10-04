5098cc7bfd058ae613bebe02e6b3d92a4e9a4052
jakegodsall <jake.edward.godsall@gmail.com>
2022-10-04 10:42:35 +0100

    reproduced logic using fetch

---
+++ js/script_fetch.js
@@ -0,0 +1,44 @@
+const button = document.getElementById('die');
+const api = 'https://api.adviceslip.com/advice';
+
+const idContainer = document.getElementById('advice-id');
+const adviceContainer = document.getElementById('advice-content');
+
+console.log(button);
+
+const getAdvice = () => {
+    const requestHeaders = {
+        Accept: 'application/json',
+    };
+
+    fetch(api, {
+        headers: requestHeaders,
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((adviceObject) => {
+            const advice = adviceObject.slip.advice;
+            const adviceID = adviceObject.slip.id;
+
+            idContainer.innerText = `Advice # ${adviceID}`;
+            adviceContainer.innerText = `"${advice}"`;
+        });
+};
+
+// make initial call of getAdvice
+window.addEventListener('click', () => {
+    getAdvice();
+});
+
+// animate button and call getAdvice
+button.addEventListener('click', (e) => {
+    if (e.target.classList.contains('waiting')) {
+        return;
+    }
+    setTimeout(() => {
+        e.target.classList.toggle('waiting');
+        getAdvice();
+    }, 3000);
+    e.target.classList.toggle('waiting');
+});
